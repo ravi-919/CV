43418c133946f0995ff3072aaf3ce841dd428019
ravi-919 <103829902+ravi-919@users.noreply.github.com>
2022-04-16 18:57:25 +0530

    US States guessing game

--- main.py
+++ main.py
@@ -1,79 +1,43 @@
-from tkinter import *
-from PIL import ImageTk, Image
+import turtle
 import pandas
-import random
-from gtts import gTTS
-import os
-import playsound
 
-BACKGROUND_COLOR = "#B1DDC6"
-current_card = {}
+screen = turtle.Screen()
+screen.title("U.S. States guessing game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
+game_is_on = True
 
-try:
-    data = pandas.read_csv("data/words_to_learn.csv")
-except FileNotFoundError:
-    original_data = pandas.read_csv("data/french_words.csv")
-    print(original_data)
-    to_learn = original_data.to_dict(orient="records")
-else:
-    to_learn = data.to_dict(orient="records")
+states_file = pandas.read_csv("50_states.csv")
+states = states_file.state
+correct_guesses = []
 
+while len(correct_guesses) < 50:
 
-def is_known():
-    to_learn.remove(current_card)
-    print(len(to_learn))
-    data1 = pandas.DataFrame(to_learn)
-    data1.to_csv("data/words_to_learn.csv", index=False)
-    next_card()
+    answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct" , prompt="What's another state's "
+                                                                                               "name?").title()
+    if answer_state == 'Exit':
+        missing_states = []
+        for item in states:
+            if item not in correct_guesses:
+                missing_states.append(item)
+        new_df = pandas.DataFrame(missing_states)
+        new_df.to_csv("states_to_learn.csv")
+        break
 
-
-def next_card():
-    language = 'fr'
-    global current_card, flip_timer
-    window.after_cancel(flip_timer)
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French", fill='black')
-    canvas.itemconfig(card_word, text=current_card['French'], fill="black")
-    canvas.itemconfig(card_background, image=card_front_img)
-    audio = gTTS(text=current_card['French'], lang=language)
-    audio.save('french.mp3')
-    playsound.playsound('/Users/Jaisinghani/Desktop/pycharm_projects/flash-card-project-start/french.mp3', True)
-    os.remove('french.mp3')
-    flip_timer = window.after(3000, func=flip_card)
+    for item in states:
+        if item == answer_state:
+            correct_guesses.append(answer_state)
+            turtle2 = turtle.Turtle()
+            turtle2.hideturtle()
+            turtle2.penup()
+            answer_state_x = float(states_file[states_file['state'] == answer_state]['x'])
+            answer_state_y = float(states_file[states_file['state'] == answer_state]['y'])
+            # print(answer_state_x)
+            # print(answer_state_y)
+            turtle2.goto(answer_state_x, answer_state_y)
+            turtle2.write(answer_state)
 
 
-def flip_card():
-    canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=current_card['English'], fill="white")
-    canvas.itemconfig(card_background, image=card_back_img)
-
-
-window = Tk()
-window.title("Flashy")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-flip_timer = window.after(3000, func=flip_card)
-
-canvas = Canvas(width=800, height=526)
-card_front_img = ImageTk.PhotoImage(Image.open("images/card_front.png"))
-card_back_img = ImageTk.PhotoImage(Image.open(('images/card_back.png')))
-card_background = canvas.create_image(400, 263, image=card_front_img)
-
-card_title = canvas.create_text(400, 150, text="", font=('Arial', 40, 'italic'))
-card_word = canvas.create_text(400, 263, text="", font=('Arial', 60, 'bold'))
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(column=0, row=0, columnspan=2)
-
-right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, border=0, command=is_known)
-right_button.grid(column=1, row=1, padx=15, pady=15)
-
-wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, border=0, command=next_card)
-wrong_button.grid(column=0, row=1, padx=15, pady=15)
-
-next_card()
-
-
-window.mainloop()
+# states_to_learn.csv
